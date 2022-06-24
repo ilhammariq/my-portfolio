b6d0e8d766c7480b0174c780ea612f9c98412e3e
ilhammariq <ilhammuhammadariq1@gmail.com>
2022-06-24 15:51:37 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0ea5e9",
+      },
+    },
+  },
+  plugins: [],
+};
