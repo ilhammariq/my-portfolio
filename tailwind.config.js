a1e48f62e0968f6c5f9d7705cd18a5b36de4180d
ilhammariq <ilhammuhammadariq1@gmail.com>
2022-06-30 16:59:48 +0700

    [Update] footer gif

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
-        primary: "#0ea5e9",
+        primary: "#FFFFFF",
+        linked: "#0072b1",
+        instagram: "#FF039C",
+        line: "#00B900",
+        wa: "#25D366",
+        github: "#171412",
       },
+    },
+    screens: {
+      "2xl": "1320px",
     },
   },
   plugins: [],
